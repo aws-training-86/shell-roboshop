82d3924b4f740a3f5fb47a5e910a75032e12f5ef
sanjay kumar <palankisanjay@gmail.com>
2025-09-29 22:21:09 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -6,19 +6,19 @@ ZONE_ID="Z10208233EZ8AOLML2P24" # replace with your ID
 #DOMAIN_NAME="daws86s.space"
 
 for instance in $@ # mongodb redis mysql
-do 
+do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-     # Get Private IP
+    # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        #RECORD_NAME="$instance.$DOMAIN_NAME" 
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        #RECORD_NAME="$DOMAIN_NAME"
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
     fi
 
-        echo "$instance: $IP"
+    echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
@@ -37,5 +37,5 @@ do
         }
         }]
     }
-    
-done 
+    '
+done
