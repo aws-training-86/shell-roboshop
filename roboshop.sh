e699a5617668906b34c063f35a6d09089d56c833
sanjay kumar <palankisanjay@gmail.com>
2025-09-30 12:17:32 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -1,41 +1,18 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0af17d18177db7565" # replace with your SG ID
-ZONE_ID="Z10208233EZ8AOLML2P24" # replace with your ID
-#DOMAIN_NAME="daws86s.space"
+SG_ID="sg-0af17d18177db7565"
 
-for instance in $@ # mongodb redis mysql
+for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    # Get Private IP
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0af17d18177db7565 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instane}]" --query 'Instances[0].InstanceId' --output text)
+
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+        IP=$(aws ec2 describe-instances --instance-ids i-055d247debcf0ab70 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+        IP=$(aws ec2 describe-instances --instance-ids i-055d247debcf0ab70 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
-    echo "$instance: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record set"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
-        }]
-    }
-    '
+    echo "$instanc: $IP"
 done
